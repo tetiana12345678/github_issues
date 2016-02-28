5a693eff3217587203292ae7eff4b4b5f9e6250d
Tetiana Hogg <tetiana.hogg@alliants.com>
2016-02-28 22:44:43 +0000

    Add json parsing of the response with jsx library Add nice formatting of the response into a table Add tests

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :jsx]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,7 +28,8 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      { :httpoison, "~> 0.4" }
+      { :httpoison, "~> 0.4" },
+      { :jsx, "~> 2.0" }
     ]
   end
 end
